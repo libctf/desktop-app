6093137e97a43244da5fe9d1c1c9b2189ce092cf
aRustyDev <36318507+aRustyDev@users.noreply.github.com>
2025-05-27 21:40:11 -0400

    feat(v0.0.0): cargo init
    swapped from swift to rust implementation

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# tmpl-swift
+# CTF-rs Desktop App
+
+## Planning
+
+- Menu
+- Resizable Windows
+- Search
+- Alerts & Toasts
+- Avatar
+- Buttons
+- Cards
+- Pagination
+-
---
+++ src/main.rs
@@ -0,0 +1,20 @@
+use cacao::appkit::menu::menu::Menu;
+use cacao::appkit::window::Window;
+use cacao::appkit::{App, AppDelegate};
+
+#[derive(Default)]
+struct BasicApp {
+    window: Window,
+}
+
+impl AppDelegate for BasicApp {
+    fn did_finish_launching(&self) {
+        self.window.set_minimum_content_size(400., 400.);
+        self.window.set_title("Hello World!");
+        self.window.show();
+    }
+}
+
+fn main() {
+    App::new("com.hello.world", BasicApp::default()).run();
+}
